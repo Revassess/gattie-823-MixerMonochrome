809acf5407cc6535d3a72558b135e96de99e1c8f
MixerMonochrome <claudiayllanes@gmail.com>
2020-08-26 15:02:29 -0400

    tier1

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -3,6 +3,15 @@ package com.revature.tier1;
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
+		String toCheck = ((Long)num).toString();
+		int len = toCheck.length();
+		int sum = 0;
+		for(int i = 0; i < len; i++) {
+			sum += Math.pow(Integer.parseInt(String.valueOf(toCheck.charAt(i))), len);
+		}
+		if(sum == num) {
+			return true;
+		}
 		return false;
 	}
 }
